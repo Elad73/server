4436e4f8744e75e52729586e146541f115640c14
EladRon <elad.ron.g@gmail.com>
2019-10-02 17:50:47 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+//route handler associated with the get method
+app.get('/', (req, res) =>  {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000; //environment variables, pass runtime configuration of the port. If doesn't exist than port = 5000 (for development environment)
+app.listen(PORT); //really node is listenning on this port
