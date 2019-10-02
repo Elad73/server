09a6f0e6141540dcac86809862c024ff401ed6d3
EladRon <elad.ron.g@gmail.com>
2019-10-02 18:41:47 +0300

    change greeting

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 
 //route handler associated with the get method
 app.get('/', (req, res) =>  {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000; //environment variables, pass runtime configuration of the port. If doesn't exist than port = 5000 (for development environment)
 app.listen(PORT); //really node is listenning on this port
+ 
