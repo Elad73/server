c10a53caef25eb2e9053856f447a192094296d93
EladRon <elad.ron.g@gmail.com>
2019-10-07 00:33:56 +0300

    adding mongoose and preparing connection to Atlas mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-require('./services/passportService')
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passportService');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-
-
-
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000; //environment variables, pass runtime configuration of the port. If doesn't exist than port = 5000 (for development environment)
 app.listen(PORT); //really node is listenning on this port
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,11 @@
-//route for authentication
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
+const passport = require('passport');
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+module.exports = app => {
+  //route for authentication
+  app.get('/auth/google', passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+};
