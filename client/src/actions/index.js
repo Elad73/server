5a18d7c5529c4070af077d670fb7338b29d838b0
EladRon <elad.ron.g@gmail.com>
2019-10-09 05:46:36 +0300

    basics of Redux Thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+//disptach an action after a success call to current_user
 const fetchUser = () => {
-    axios.get('/api/current_user');
+    return function(dispatch) {
+        axios.get('/api/current_user')
+            .then(res => dispatch( {type: FETCH_USER, payload: res}));
+    }
 
 };
