1b247c44a1f330d28f2d4b5259e24222a5a7ca9c
EladRon <elad.ron.g@gmail.com>
2020-01-02 22:52:52 +0200

    Reusing Action Types

--- config/prod.js
+++ config/prod.js
@@ -4,8 +4,8 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECERT,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  stripePubhlishableKey: process.STRIPE_PUBLISHABLE_KEY,
-  stripeSecertKey: process.STRIPE_SECERT_KEY
+  stripePublishableKey: process.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.STRIPE_SECRET_KEY
 };
 
 
