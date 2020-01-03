feea0d03de72450ac1c03a4b0447107c1c96aa54
EladRon <elad.ron.g@gmail.com>
2020-01-03 11:26:13 +0200

    Adding Credits to User

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const { Schema } = mongoose;
 
 //can freely add or subtract propetries as needed 
 const userSchema = new Schema({
-  googleID: String
+  googleID: String,
+  credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -10,7 +10,10 @@ module.exports = app => {
             source: req.body.id
         });
 
-        console.log(charge);
+        req.user.credits += 5;
+        const user = await req.user.save();
+
+        res.send(user);
     });
 };
   
