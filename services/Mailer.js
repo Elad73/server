717b42ace4ff886f5c6e602a0cc8c2f6a2b9649b
EladRon <elad.ron.g@gmail.com>
2020-01-09 13:13:33 +0200

    Mailer Constructor

--- services/Mailer.js
+++ services/Mailer.js
@@ -3,8 +3,15 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
 
-}
-
+        //sendgrid specigic setup
+        this.from_email = new helper.Email('no-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
+};
 
 module.exports = Mailer;
