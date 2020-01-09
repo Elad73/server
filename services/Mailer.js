0740b59a06064560a2fa7108f3aff114f832b264
EladRon <elad.ron.g@gmail.com>
2020-01-09 20:13:32 +0200

    More Mailer Properties

--- services/Mailer.js
+++ services/Mailer.js
@@ -30,6 +30,14 @@ class Mailer extends helper.Mail {
         trackingSettings.setClickTracking(clickTracking);
         this.addTrackingSettings(trackingSettings);
     }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.foreach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
 }
 
 module.exports = Mailer;
