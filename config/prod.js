cbee47c7f6041b9a5f14d4953e694978f1ad046d
EladRon <elad.ron.g@gmail.com>
2020-01-09 12:17:30 +0200

    sendgrid setup

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.STRIPE_SECRET_KEY
+  stripeSecretKey: process.STRIPE_SECRET_KEY,
+  sendGridKey: process.SEND_GRID_KEY
 };
 
 
