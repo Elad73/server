62f53285bcbe422918a2fb822bf2d57dcd98e756
EladRon <elad.ron.g@gmail.com>
2020-01-09 11:23:25 +0200

    Creating Subdoc Collection

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,6 +9,12 @@ module.exports = app => {
         const { title, subject, body, recipients }  = req.body;
 
         const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map( email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now(),
 
         });
     });  
