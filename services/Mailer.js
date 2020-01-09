fe15a1aaf704fc748337f6a57ff9e87954d2fb11
EladRon <elad.ron.g@gmail.com>
2020-01-09 20:05:57 +0200

    Bilerplate for Sending Emails

--- services/Mailer.js
+++ services/Mailer.js
@@ -6,12 +6,30 @@ class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content) {
         super();
 
-        //sendgrid specigic setup
+        //sendgrid specific setup
         this.from_email = new helper.Email('no-reply@emaily.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
-        this.recipients = this.formatAddresses(recipients);
+        this.recipients = this.formatAddresses(recipients); //array of formated emails of the recipients
+
+        this.addContent(this.body); //this is build-in function from helper.Mail
+        this.addClickTracking();
+        this.addRecipients();
     }
-};
+
+    formatAddresses(recipients){
+        return recipients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.clickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+}
 
 module.exports = Mailer;
