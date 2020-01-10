0a50c07c2880adfd76f0bb62849cdb6d5344268d
EladRon <elad.ron.g@gmail.com>
2020-01-10 14:12:16 +0200

    Material Icons

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Dashboard = () => {
   return (
-      <div style={{ textAlign: 'center'}}>
-          <h1>
-              The most stunning Dashboard EVER!
-          </h1>
-          Images, bank status, dreams track, goals track and much more...
-          <br></br>
-          This is a game changer that will change your'e life to levels you haven't met before.
+      <div>
+          Dashboard
+          <div className="fixed-action-btn">
+              <a className="btn-floating btn-large red">
+                  <i className="material-icons">add</i>
+              </a>
+          </div>
       </div>
   );
 };
