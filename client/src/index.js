43afa8470f895d4ba753d63d8b48abf63b4d012b
EladRon <elad.ron.g@gmail.com>
2020-01-10 06:45:45 +0200

    Improving the Email Template

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
+
+//Development only axios helpers!
 import axios from 'axios';
 window.axios = axios;
 
