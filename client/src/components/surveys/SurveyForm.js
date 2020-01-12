999007e917fc093cd267ffbf28e0a72e256db443
EladRon <elad.ron.g@gmail.com>
2020-01-12 16:34:27 +0200

    Dumping Form Values

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -21,7 +21,7 @@ class SurveyForm extends Component {
             <div>
                 <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
                     {this.renderFields()}
-                    <Link to="/surveys" className="red btn-flat left white-text">
+                    <Link to="/dashboard" className="red btn-flat left white-text">
                         Cancel
                         <i className="material-icons right">cancel</i>
                     </Link>
