b77782714dd64405e6bedec2b395893dfb9df547
EladRon <elad.ron.g@gmail.com>
2020-01-12 08:28:26 +0200

    Retreat to the Form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -7,10 +7,10 @@ class SurveyNew extends Component {
 
     renderContent() {
         if (this.state.showFormReview) {
-            return <SurveyFormReview />;
+            return <SurveyFormReview onCancel={() => this.setState({ showFormReview: false })} />;
         }
 
-        return <SurveyForm onSurveySubmit={ () => this.setState({ showFormReview: true }) } />;
+        return <SurveyForm onSurveySubmit={() => this.setState({ showFormReview: true })} />;
     }
 
     render() {
