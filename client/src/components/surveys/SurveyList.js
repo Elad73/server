6667375d77a07b6ce176f4a16bbf6e3fed07b47f
EladRon <elad.ron.g@gmail.com>
2020-01-15 13:05:07 +0200

    Reversing the Survey List

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,9 +8,7 @@ class SurveyList extends Component {
     }
 
     renderSurveys() {
-        
-        return this.props.surveys.map(survey => {
-            console.log(survey.dateSent);
+        return this.props.surveys.reverse().map(survey => {
             return (
                 <div className="card blue-grey darken-1" key={survey._id}>
                     <div className="card-content">
@@ -19,13 +17,12 @@ class SurveyList extends Component {
                             {survey.body}
                         </p>
                         <p className="right">
-                            
                             Send On: {new Date(survey.dateSent).toLocaleDateString()}
                         </p>
                     </div>
                     <div className="card-action">
                         <a>Yes {survey.yes}</a>
-                        <a>No {survey.yes}</a>
+                        <a>No {survey.no}</a>
                     </div>
                 </div>
             );
