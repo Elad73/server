843f677ea25dd9558a3458097096e8b88bac7e96
EladRon <elad.ron.g@gmail.com>
2020-01-15 07:15:37 +0200

    Fetching a List of Surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,68 +10,74 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-        const { title, subject, body, recipients }  = req.body;
+const requireLogin = require('../middlewares/requireLogin');
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id });
 
-        const survey = new Survey({
-            title,
-            subject,
-            body,
-            recipients: recipients.split(',').map( email => ({ email: email.trim() })),
-            _user: req.user.id,
-            dateSent: Date.now(),
-        });
+    res.send(surveys);
+  });
 
-        //Great place to send an email!
-        const mailer = new Mailer(survey, surveyTemplate(survey));
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients }  = req.body;
 
-        try {
-            await mailer.send();
-
-            await survey.save();
-            req.user.credits -= 1;
-            const user = await req.user.save();
-
-            res.send(user);
-        } catch(err){
-            res.status(422).send(err); //unprocessable entity
-        }
+    const survey = new Survey({
+        title,
+        subject,
+        body,
+        recipients: recipients.split(',').map( email => ({ email: email.trim() })),
+        _user: req.user.id,
+        dateSent: Date.now(),
     });
 
-    app.post('/api/surveys/webhooks', (req, res) => {
-        const p = new Path('/api/surveys/:surveyId/:choice');
+    //Great place to send an email!
+    const mailer = new Mailer(survey, surveyTemplate(survey));
 
-        const events = _.chain(req.body)
-            .map(({ url, email}) => {
-                    const match = p.test(new URL(url).pathname); //either will be an object (with a surveyId and a choice) or a null
-                    if (match) {
-                        return { email, surveyId: match.surveyId, choice: match.choice };
-                    }
-            }) 
-            .compact() //return only events objects, removing all of the undefined
-            .uniqBy('email', 'surveyId') //if there are any duplicants at the email or at the surveyId fields, then remove them
-            .each(({ surveyId, email, choice }) => {
-                Survey.updateOne(
-                  {
-                    _id: surveyId,
-                    recipients: {
-                      $elemMatch: { email: email, responded: false }
-                    }
-                  },
-                  {
-                    $inc: { [choice]: 1 },
-                    $set: { 'recipients.$.responded': true },
-                    lastResponded: new Date()
-                  }
-                ).exec();
-              })
-            .value();
+    try {
+        await mailer.send();
 
-        res.send({});
-    });
+        await survey.save();
+        req.user.credits -= 1;
+        const user = await req.user.save();
 
-    
-    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
-        res.send('Thanks for voting!');
-    }); 
+        res.send(user);
+    } catch(err){
+        res.status(422).send(err); //unprocessable entity
+    }
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = _.chain(req.body)
+        .map(({ url, email}) => {
+                const match = p.test(new URL(url).pathname); //either will be an object (with a surveyId and a choice) or a null
+                if (match) {
+                    return { email, surveyId: match.surveyId, choice: match.choice };
+                }
+        }) 
+        .compact() //return only events objects, removing all of the undefined
+        .uniqBy('email', 'surveyId') //if there are any duplicants at the email or at the surveyId fields, then remove them
+        .each(({ surveyId, email, choice }) => {
+            Survey.updateOne(
+              {
+                _id: surveyId,
+                recipients: {
+                  $elemMatch: { email: email, responded: false }
+                }
+              },
+              {
+                $inc: { [choice]: 1 },
+                $set: { 'recipients.$.responded': true },
+                lastResponded: new Date()
+              }
+            ).exec();
+          })
+        .value();
+
+    res.send({});
+  });
+  
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
+    res.send('Thanks for voting!');
+  }); 
 };
