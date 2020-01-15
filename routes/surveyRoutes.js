76f7b7ca45baef8bfa8a315e8db933fa74c5fe31
EladRon <elad.ron.g@gmail.com>
2020-01-15 08:36:40 +0200

    Whitelisting Model Fields

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,7 +12,8 @@ const Survey = mongoose.model('surveys');
 module.exports = app => {
 const requireLogin = require('../middlewares/requireLogin');
   app.get('/api/surveys', requireLogin, async (req, res) => {
-    const surveys = await Survey.find({ _user: req.user.id });
+    const surveys = await Survey.find({ _user: req.user.id })
+      .select({ recipients: false });
 
     res.send(surveys);
   });
