6f7a18a8c9462c7ca3d1767ef26eec5ca5c34b85
EladRon <elad.ron.g@gmail.com>
2020-01-15 12:53:18 +0200

    Rendering a List of Surveys

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -3,9 +3,41 @@ import { connect } from 'react-redux';
 import { fetchSurveys } from '../../actions' ;
 
 class SurveyList extends Component {
+    componentDidMount() {
+        this.props.fetchSurveys();
+    }
+
+    renderSurveys() {
+        
+        return this.props.surveys.map(survey => {
+            console.log(survey.dateSent);
+            return (
+                <div className="card blue-grey darken-1" key={survey._id}>
+                    <div className="card-content">
+                        <span className="card-title">{survey.title}</span>
+                        <p>
+                            {survey.body}
+                        </p>
+                        <p className="right">
+                            
+                            Send On: {new Date(survey.dateSent).toLocaleDateString()}
+                        </p>
+                    </div>
+                    <div className="card-action">
+                        <a>Yes {survey.yes}</a>
+                        <a>No {survey.yes}</a>
+                    </div>
+                </div>
+            );
+        });
+    }
+
+
     render() {
         return (
-           <div>SurveyList</div> 
+           <div>
+               {this.renderSurveys()}
+           </div> 
         );    
     }
 }
@@ -15,3 +47,4 @@ function mapStateToProps({ surveys }) {
 }
 
 export default connect(mapStateToProps, { fetchSurveys })(SurveyList);
+
