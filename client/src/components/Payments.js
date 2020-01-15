d031ec3fa06565fb4f932910ccbfa12b059f60e0
EladRon <elad.ron.g@gmail.com>
2020-01-15 16:47:02 +0200

    replacing back the react env key for stripe

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -11,7 +11,7 @@ class Payments extends Component {
                 description="$5 for 5 email credits"
                 amount={500}
                 token={token => this.props.handleToken(token)}
-                stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
             >
                 <button className="btn">
                     Add Credits
