cf9156daa99171e4aa560be5733a48e918170592
EladRon <elad.ron.g@gmail.com>
2020-01-16 06:41:40 +0200

    adding logs to Mailer.js

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,15 +8,18 @@ class Mailer extends helper.Mail {
 
         //sendgrid specific setup
         this.sgApi = sendgrid(keys.sendGridKey);
-        console.log("this.sgApi---------------------------------------------->" + this.sgApi);
+        console.log("this.sgApi---------------------------------------------->" + JSON.stringify(this.sgApi));
         this.from_email = new helper.Email('no-reply@emaily.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recipients = this.formatAddresses(recipients); //array of formated emails of the recipients
 
         this.addContent(this.body); //this is build-in function from helper.Mail
+        console.log("After this.addContent---------------------------------------------->");
         this.addClickTracking();
+        console.log("After this.addClickTracking---------------------------------------------->");
         this.addRecipients();
+        console.log("After this.addRecipients---------------------------------------------->");
     }
 
     formatAddresses(recipients){
@@ -42,6 +45,7 @@ class Mailer extends helper.Mail {
     }
 
     async send() {
+        console.log("Inside send making the post request---------------------------------------------->");
         const request = this.sgApi.emptyRequest({
             method: 'POST',
             path: '/v3/mail/send',
@@ -49,6 +53,7 @@ class Mailer extends helper.Mail {
         });
 
         const response = await this.sgApi.API(request);
+        console.log("After response ---------------------------------------------->" + JSON.stringify(response));
         return response;
     }
 }
